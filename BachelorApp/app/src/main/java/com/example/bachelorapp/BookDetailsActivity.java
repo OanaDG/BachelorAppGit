03b6e3cfb57719c95bff716377909667de249e28
Madalin Ghita <madalin.ghita@sbs.local>
2021-04-13 18:17:16 +0300

    add to cart

--- BachelorApp/app/src/main/java/com/example/bachelorapp/BookDetailsActivity.java
+++ BachelorApp/app/src/main/java/com/example/bachelorapp/BookDetailsActivity.java
@@ -9,9 +9,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.bachelorapp.Collection.Collection;
 import com.example.bachelorapp.Model.Books;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +23,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class BookDetailsActivity extends AppCompatActivity {
 
@@ -57,7 +65,55 @@ public class BookDetailsActivity extends AppCompatActivity {
             }
         });
 
+        btnAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addToCart();
+            }
+        });
+
         displayBookDetails(bookId);
+
+    }
+
+    private void addToCart() {
+        String currentTime, currentDate;
+
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+        currentDate = dateFormat.format(date.getTime());
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss a");
+        currentTime = dateFormat.format(date.getTime());
+
+        final DatabaseReference cartRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("id", bookId);
+        cartMap.put("title", tvTitle.getText().toString());
+        cartMap.put("author", tvAuthor.getText().toString());
+        cartMap.put("price", tvPrice.getText().toString());
+        cartMap.put("date", currentDate);
+
+        cartRef.child("User View").child(Collection.currentUser.getUsername()).child("Products").child(bookId).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    cartRef.child("Admin View").child(Collection.currentUser.getUsername()).child("Products").child(bookId).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(BookDetailsActivity.this, "Added to the cart", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(BookDetailsActivity.this, HomeActivity.class);
+                                intent.putExtra("category", category);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
     }
 
