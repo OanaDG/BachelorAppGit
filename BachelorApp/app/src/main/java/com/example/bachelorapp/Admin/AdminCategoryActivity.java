e1366c6b8a49907c487495a6513bd73acef038a1
Madalin Ghita <madalin.ghita@sbs.local>
2021-06-07 11:46:47 +0300

    version with report

--- BachelorApp/app/src/main/java/com/example/bachelorapp/Admin/AdminCategoryActivity.java
+++ BachelorApp/app/src/main/java/com/example/bachelorapp/Admin/AdminCategoryActivity.java
@@ -1,21 +1,39 @@
 package com.example.bachelorapp.Admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.bachelorapp.Model.Orders;
 import com.example.bachelorapp.User.HomeActivity;
 import com.example.bachelorapp.User.LoginActivity;
 import com.example.bachelorapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 
+    private static final String TAG = AdminCategoryActivity.class.getSimpleName();
     ImageView imgBestseller, imgDiscount, imgAll, tvLogout;
-    Button btnManageOrders, btnProductMaintenance;
+    Button btnManageOrders, btnProductMaintenance, btnCustomerPurchasesReport;
+    StringBuilder builder = new StringBuilder();
+    DatabaseReference ordersRef, usersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +46,35 @@ public class AdminCategoryActivity extends AppCompatActivity {
         imgAll = findViewById(R.id.imgAll);
         btnManageOrders = findViewById(R.id.btnCheckOrders);
         btnProductMaintenance = findViewById(R.id.btnMaintenance);
+        btnCustomerPurchasesReport = findViewById(R.id.btnClientReport);
+        builder.append("Customer,Revenue(RON)");
+
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+        ordersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Map<String, Object> map = (Map<String, Object>)snapshot.getValue();
+                for(Map.Entry<String, Object> pair : map.entrySet()) {
+                    String name = pair.getKey();
+                    builder.append("\n" + name + ",");
+                    DataSnapshot allSnapshots = snapshot.child(name);
+                    Iterable<DataSnapshot> orderChildren = allSnapshots.getChildren();
+                    double revenue = 0;
+                    for (DataSnapshot snap : orderChildren) {
+                        Orders order = snap.getValue(Orders.class);
+                        revenue += Double.valueOf(order.getTotalPrice());
+
+                    }
+                    builder.append(revenue);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         imgBestseller.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,5 +131,36 @@ public class AdminCategoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        //to do method for generating csv
+        btnCustomerPurchasesReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: " + builder.toString());
+                generateRevenueByClientReport();
+            }
+        });
+    }
+
+    public void generateRevenueByClientReport() {
+        try{
+            FileOutputStream out = openFileOutput("revenueReport.csv", Context.MODE_PRIVATE);
+            out.write((builder.toString()).getBytes());
+            out.close();
+
+            Context context = getApplicationContext();
+            File file = new File(getFilesDir(), "revenueReport.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.bachelorapp.fileprovider", file);
+            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+            fileIntent.setType("text/csv");
+            fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Revenue brought by each client");
+            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+            startActivity(Intent.createChooser(fileIntent, "Save report"));
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
